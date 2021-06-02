d6763ff28833201cfc8ddb852f94613269c43fd9
sebauribeo <chefsebaurib@gmail.com>
2021-06-02 14:21:49 -0400

    capa de errores

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import { Message } from '@google-cloud/pubsub';
 import * as circularJSON from "circular-json";
 import { EventPattern } from '@nestjs/microservices';
 import { RedisService } from "./services/redis/redis.service";
+import { response } from 'express';
 
 @Controller()
 export class AppController {
@@ -12,15 +13,15 @@ export class AppController {
     private loggerService: LoggerService,
     private redisService: RedisService,
     ){};
-  
+    
     @EventPattern(process.env.GCLOUD_SUBSCRIPTION_NAME)
     async messageHandler(message: Message){
         const data = message.data ? message.data.toString() : null;
         const validationResult: ValidationDTO = JSON.parse(data); 
 
-        this.loggerService.customInfo({}, { 'Data from the server...': circularJSON.parse(data) });
-        this.loggerService.customInfo({}, { 'Data send to Redis!...': message.id });
-        await this.redisService.saveData(validationResult.id.toString(), JSON.stringify(validationResult));
+        this.loggerService.customInfo({}, { 'Data send to Redis, id': message.id });
+        this.loggerService.customInfo({}, { 'Data from the server...': circularJSON.parse(data)});
+        await this.redisService.saveData(validationResult.id.toString(), JSON.stringify(validationResult), response);
 
         message.ack();
     };
--- src/services/redis/redis.service.ts
+++ src/services/redis/redis.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, HttpStatus, Response } from '@nestjs/common';
 import { validate } from 'class-validator';
 import { ValidationDTO } from 'src/dto/validation.dto';
 import { LoggerService } from '../logger/logger.service';
@@ -16,7 +16,7 @@ export class RedisService {
         private readonly loggerService: LoggerService,
     ){}
     
-    async saveData(key: any, value: any){
+    async saveData(key: any, value: any, @Response() response){
         const data: any = await redisClient.set(key, value);
         const validationResult: ValidationDTO = data; 
         const result = new ValidationDTO(validationResult);
@@ -25,11 +25,14 @@ export class RedisService {
         if (data === null){
             this.loggerService.customError(null, validation);
             this.loggerService.customError({}, {message: 'Data Error!...', id: key});
-            return (Error);
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: 'Data Redis Not Found',
+            }, HttpStatus.NOT_FOUND);
         } else {
-            this.loggerService.customInfo({}, { message: 'The subscribed message has been consumed...' });
             this.loggerService.customInfo({}, {message: 'Message sent!...'});
-            return (data) 
-        }
+            this.loggerService.customInfo({}, { message: 'The subscribed message has been consumed...' });
+            return response.status(HttpStatus.OK).data
+        };
     };
 }; 
